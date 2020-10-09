c9f191388fd701aeae1077d2b75289fe265f62ad
ryzzaki <cuong.nugi@gmail.com>
2020-10-09 18:45:46 +0200

    chore: add a procfile

--- src/components/slices/CodeSnippet.tsx
+++ src/components/slices/CodeSnippet.tsx
@@ -14,8 +14,8 @@ const CodeSnippet: React.FC<CodeSnippetProps> = ({
   }, []);
 
   return (
-    <div className="rounded-md bg-darkgrey my-10 p-5 text-14">
-      <Markdown source={markdownContent} className="line-numbers" />
+    <div className="rounded-lg bg-darkgrey my-10 p-5 text-14">
+      <Markdown source={markdownContent} />
     </div>
   );
 };
