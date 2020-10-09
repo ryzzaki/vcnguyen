c23bd3ca00465654feb646436d8ebd57f19921e5
ryzzaki <cuong.nugi@gmail.com>
2020-10-09 18:14:01 +0200

    update: projects page simplification

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,19 +1,15 @@
 import MainContainer from '../../components/containers/MainContainer';
 import React, { useEffect, useState } from 'react';
-import TechStackList from '../../components/TechStackList';
 import OrganizationList from '../../components/OrganizationList';
 import { PrismicClient } from '../api/prismic';
-import { RichText } from 'prismic-reactjs';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
 import BodyText from '../../components/slices/BodyText';
 
 const ProjectsPage: React.FC = () => {
   const [data, setData] = useState({
-    intro_title: [],
-    intro_description: [],
-    work_projects_title: [],
-    solo_projects_title: [],
-    hackathon_projects_title: [],
-    concept_projects_title: [],
+    intro_title: [] as RichTextBlock[],
+    intro_description: [] as RichTextBlock[],
+    projects_title: [] as RichTextBlock[],
   });
 
   useEffect(() => {
@@ -23,10 +19,7 @@ const ProjectsPage: React.FC = () => {
       setData({
         intro_title: responseData?.intro_title,
         intro_description: responseData?.intro_description,
-        work_projects_title: responseData?.work_projects_title,
-        solo_projects_title: responseData?.solo_projects_title,
-        hackathon_projects_title: responseData?.hackathon_projects_title,
-        concept_projects_title: responseData?.concept_projects_title,
+        projects_title: responseData?.projects_title,
       });
     });
   }, []);
@@ -36,7 +29,7 @@ const ProjectsPage: React.FC = () => {
       <div className="flex flex-col items-left mx-auto">
         <section className="py-20 my-20">
           <div className="font-semibold py-10 text-24 md:text-32">
-            {RichText.render(data.intro_title)}
+            <RichText render={data.intro_title} />
           </div>
           <div className="py-10 text-18 md:text-24">
             <BodyText text={data.intro_description} />
@@ -44,25 +37,7 @@ const ProjectsPage: React.FC = () => {
         </section>
         <section className="pb-20 my-20">
           <div className="font-semibold py-10 text-24 md:text-32">
-            {RichText.render(data.work_projects_title)}
-          </div>
-          <TechStackList />
-        </section>
-        <section className="pb-20 my-20">
-          <div className="font-semibold py-10 text-24 md:text-32">
-            {RichText.render(data.solo_projects_title)}
-          </div>
-          <OrganizationList />
-        </section>
-        <section className="pb-20 my-20">
-          <div className="font-semibold py-10 text-24 md:text-32">
-            {RichText.render(data.hackathon_projects_title)}
-          </div>
-          <OrganizationList />
-        </section>
-        <section className="pb-20 my-20">
-          <div className="font-semibold py-10 text-24 md:text-32">
-            {RichText.render(data.concept_projects_title)}
+            <RichText render={data.projects_title} />
           </div>
           <OrganizationList />
         </section>
