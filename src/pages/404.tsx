5d8fe59a96a42925aaedee65708ebbfa44d0f188
ryzzaki <cuong.nugi@gmail.com>
2020-10-10 14:47:01 +0200

    feat: custom error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import MainContainer from '../components/containers/MainContainer';
+import React from 'react';
+
+const NotFoundErrorPage: React.FC = () => (
+  <MainContainer title="Page Not Found :( | Cuong Nguyen" theme="dark">
+    <div className="text-center text-lightblue font-heading text-26 md:text-34 p-10">
+      FOUR-O'-FOUR
+    </div>
+    <div className="text-center font-heading text-26 md:text-34 p-10">
+      This page does not exist 😭
+    </div>
+    <div className="text-center font-heading text-26 md:text-34 p-10 pt-30">
+      (╯°□°)╯︵ ┻━┻
+    </div>
+  </MainContainer>
+);
+
+export default NotFoundErrorPage;
