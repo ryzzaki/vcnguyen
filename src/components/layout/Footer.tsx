591c18a50bd3692db0d173be4ff3abfdc48e2e0f
ryzzaki <cuong.nugi@gmail.com>
2020-10-10 11:26:36 +0200

    chore: rewrite footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,20 +2,30 @@ import React from 'react';
 import Link from 'next/link';
 
 const Footer: React.FC = () => (
-  <footer className="flex flex-col bg-mirage text-center items-center w-full font-roboto py-25 px-10">
+  <footer className="flex flex-col bg-mirage text-center items-center w-full py-25 px-10">
     <div className="font-semibold text-white text-14">
       Wow you made it to the very end 😮
     </div>
     <div className="text-lightgrey text-14">
-      &copy; 2020 by Cuong Nguyen. All rights reserved. |{' '}
+      &copy; 2020 by{' '}
       <Link href="https://www.instagram.com/cng_ng/">
         <a
           target="_blank"
           className="hover:text-lightblue transition duration-300 ease-in-out"
         >
-          @cng_ng
+          Cuong Nguyen
         </a>
       </Link>
+      . Colour Palette by{' '}
+      <Link href="https://www.instagram.com/liolio.ness/">
+        <a
+          target="_blank"
+          className="hover:text-lightblue transition duration-300 ease-in-out"
+        >
+          Julie Nguyen Lev
+        </a>
+      </Link>
+      . All rights reserved.
     </div>
     <div className="p-15 md:p-0 mt-10 md:mt-0 text-lightgrey text-10">
       MIT Licensed | Made with ♥️ using Next.js
